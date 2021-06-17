80d09e9397cc816cb1adc0b32d2b0adb4444e2e2
guilherme <guilherme.godoi@allcomsistemas.com.br>
2021-06-17 14:58:18 -0300

    :wrench: Adicionado eslint e prettier

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,11 @@ import express from "express";
 
 const app = express();
 
-/**
- *
- * Midleware
- */
+// Midleware
 app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello World!" });
 });
 
-app.listen(3333, () => console.log("Init Server on localhost:3333"));
+app.listen(3333, () => console.log("Init Server on http://localhost:3333"));
